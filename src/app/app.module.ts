cddf2de6d8e992f91ed7a0a2ff337b3fc10eedb3
Sander van Broekhoven <s.vanbroekhoven@isah.com>
2019-10-11 10:15:26 +0200

    remove routing and add capture component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CaptureComponent } from './capture/capture.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CaptureComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
